8d43d38ca4d874c7823054d431acfec69da218ee
Tom Alabaster <tom.alabaster@nakedwines.com>
2022-11-18 19:49:29 +0000

    Recognizing models

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
+import 'dart:io' as io;
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_mlkit_image_labeling/google_mlkit_image_labeling.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 
 late List<CameraDescription> _cameras;
 
@@ -38,6 +43,8 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   late CameraController controller;
 
+  CameraImage? _cameraImage;
+
   @override
   void initState() {
     super.initState();
@@ -47,6 +54,9 @@ class _MyHomePageState extends State<MyHomePage> {
       if (!mounted) {
         return;
       }
+
+      controller.startImageStream((image) => _cameraImage = image);
+
       setState(() {});
     }).catchError((Object e) {
       if (e is CameraException) {
@@ -76,7 +86,82 @@ class _MyHomePageState extends State<MyHomePage> {
           title: Text(widget.title),
         ),
         body: controller.value.isInitialized
-            ? CameraPreview(controller)
+            ? FittedBox(
+                child: GestureDetector(
+                  onTap: () {
+                    _processImage();
+                  },
+                  child: CameraPreview(controller),
+                ),
+              )
             : const SizedBox.shrink());
+  }
+
+  Future<String> _getModel(String assetPath) async {
+    if (io.Platform.isAndroid) {
+      return 'flutter_assets/$assetPath';
+    }
+    final path = '${(await getApplicationSupportDirectory()).path}/$assetPath';
+    await io.Directory(dirname(path)).create(recursive: true);
+    final file = io.File(path);
+    if (!await file.exists()) {
+      final byteData = await rootBundle.load(assetPath);
+      await file.writeAsBytes(byteData.buffer
+          .asUint8List(byteData.offsetInBytes, byteData.lengthInBytes));
+    }
+    return file.path;
+  }
+
+  void _processImage() async {
+    final WriteBuffer allBytes = WriteBuffer();
+    for (final Plane plane in _cameraImage!.planes) {
+      allBytes.putUint8List(plane.bytes);
+    }
+    final bytes = allBytes.done().buffer.asUint8List();
+
+    final Size imageSize =
+        Size(_cameraImage!.width.toDouble(), _cameraImage!.height.toDouble());
+
+    final InputImageRotation imageRotation =
+        InputImageRotationValue.fromRawValue(
+            controller.description.sensorOrientation)!;
+
+    final InputImageFormat inputImageFormat =
+        InputImageFormatValue.fromRawValue(_cameraImage!.format.raw)!;
+
+    final planeData = _cameraImage!.planes.map(
+      (Plane plane) {
+        return InputImagePlaneMetadata(
+          bytesPerRow: plane.bytesPerRow,
+          height: plane.height,
+          width: plane.width,
+        );
+      },
+    ).toList();
+
+    final inputImageData = InputImageData(
+      size: imageSize,
+      imageRotation: imageRotation,
+      inputImageFormat: inputImageFormat,
+      planeData: planeData,
+    );
+
+    final inputImage =
+        InputImage.fromBytes(bytes: bytes, inputImageData: inputImageData);
+
+    final modelPath = await _getModel('assets/ml/model.tflite');
+    final options = LocalLabelerOptions(modelPath: modelPath);
+    final imageLabeler = ImageLabeler(options: options);
+    final List<ImageLabel> labels = await imageLabeler.processImage(inputImage);
+
+    for (ImageLabel label in labels) {
+      final String text = label.label;
+      final int index = label.index;
+      final double confidence = label.confidence;
+
+      print('$text $index $confidence');
+    }
+
+    await imageLabeler.close();
   }
 }
